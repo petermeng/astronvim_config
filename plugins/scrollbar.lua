0c64ab10d66b7015e4d38061d23fc0985b26620e
petermeng_01@163.com <petermeng_01@163.com>
2023-07-21 22:49:14 +0800

    modify scrollbar plugin

--- plugins/scrollbar.lua
+++ plugins/scrollbar.lua
@@ -1,25 +1,26 @@
 return {
-	"petertriho/nvim-scrollbar",
-	config = function()
-		require("scrollbar.handlers.search").setup()
-		require("scrollbar").setup({
-			show = true,
-			handle = {
-				text = " ",
-				color = "#928374",
-				hide_if_all_visible = true,
-			},
-			marks = {
-				Search = { color = "yellow" },
-				Misc = { color = "purple" },
-			},
-			handlers = {
-				cursor = false,
-				diagnostic = true,
-				gitsigns = true,
-				handle = true,
-				search = true,
-			},
-		})
-	end,
+  "petertriho/nvim-scrollbar",
+  lazy = false,
+  config = function()
+    require("scrollbar.handlers.search").setup()
+    require("scrollbar").setup({
+      show = true,
+      handle = {
+        text = " ",
+        color = "#928374",
+        hide_if_all_visible = true,
+      },
+      marks = {
+        Search = { color = "yellow" },
+        Misc = { color = "purple" },
+      },
+      handlers = {
+        cursor = false,
+        diagnostic = true,
+        gitsigns = true,
+        handle = true,
+        search = true,
+      },
+    })
+  end,
 }
