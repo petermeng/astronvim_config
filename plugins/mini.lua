42163a4a8ae2aef1c36f0d02197a77a259d47a27
petermeng_01@163.com <petermeng_01@163.com>
2023-07-22 15:38:25 +0800

    update configuration

--- plugins/mini.lua
+++ plugins/mini.lua
@@ -2,6 +2,7 @@ return {
     {
         'echasnovski/mini.animate',
         version = false,
+        lazy = false,
         config = function()
             require('mini.animate').setup()
         end
@@ -13,5 +14,21 @@ return {
         config = function()
             require('mini.move').setup()
         end,
+    },
+    {
+        'echasnovski/mini.align',
+        lazy = false,
+        version = false,
+        config = function()
+            require('mini.align').setup()
+        end
+    },
+    {
+        'echasnovski/mini.indentscope',
+        lazy = false,
+        version = false,
+        config = function()
+            require('mini.indentscope').setup()
+        end
     },
 }
